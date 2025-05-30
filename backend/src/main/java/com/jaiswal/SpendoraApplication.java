3ab809db7186bbd276bd5b6e176b45c63d24db53
Pooja Jaiswal <jaiswal.pooja1809@gmail.com>
2025-05-30 23:33:59 +0530

    add SpendoraApplication

--- backend/src/main/java/com/jaiswal/SpendoraApplication.java
+++ backend/src/main/java/com/jaiswal/SpendoraApplication.java
@@ -2,12 +2,22 @@ package com.jaiswal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-public class BackendApplication {
+@EnableCaching
+@EnableMongoAuditing
+@EnableAsync
+@EnableTransactionManagement
+public class SpendoraApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		System.setProperty("spring.profiles.default", "dev");
+		SpringApplication app = new SpringApplication(SpendoraApplication.class);
+		app.setAdditionalProfiles("dev");
+		app.run(args);
 	}
-
 }
