20ddf75263d8a2478f37ae87cc6368dba259adcc
Pooja Jaiswal <jaiswal.pooja1809@gmail.com>
2025-05-30 23:48:11 +0530

    add UserDTO

--- backend/src/main/java/com/jaiswal/model/dto/UserDTO.java
+++ backend/src/main/java/com/jaiswal/model/dto/UserDTO.java
@@ -1,1 +1,61 @@
- 
+package com.jaiswal.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+
+    private String id;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    private String profileImageUrl;
+
+    private Set<String> roles;
+
+    private UserPreferencesDTO preferences;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime lastLoginAt;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserPreferencesDTO {
+        private String currency;
+        private String dateFormat;
+        private String timezone;
+        private Boolean emailNotifications;
+        private Boolean pushNotifications;
+        private String theme;
+    }
+}
+
