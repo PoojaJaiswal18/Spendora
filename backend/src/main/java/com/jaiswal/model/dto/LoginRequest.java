f8868d03b0a1a2cc3b7eb41b42d197ae7e9812f0
Pooja Jaiswal <jaiswal.pooja1809@gmail.com>
2025-05-30 23:50:32 +0530

    add LoginRequest

--- backend/src/main/java/com/jaiswal/model/dto/LoginRequest.java
+++ backend/src/main/java/com/jaiswal/model/dto/LoginRequest.java
@@ -1,1 +1,24 @@
- 
+package com.jaiswal.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    @NotBlank(message = "Username or email is required")
+    private String usernameOrEmail;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    private Boolean rememberMe = false;
+}
+
