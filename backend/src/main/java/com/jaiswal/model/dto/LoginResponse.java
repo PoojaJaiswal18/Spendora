747d1723f766b80f9e6dac2f75bae8e0bc2aad7a
Pooja Jaiswal <jaiswal.pooja1809@gmail.com>
2025-05-30 23:51:10 +0530

    add LoginResponse

--- backend/src/main/java/com/jaiswal/model/dto/LoginResponse.java
+++ backend/src/main/java/com/jaiswal/model/dto/LoginResponse.java
@@ -1,1 +1,23 @@
- 
+package com.jaiswal.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType = "Bearer";
+    private Long expiresIn;
+    private UserDTO user;
+    private LocalDateTime loginTime;
+}
+
