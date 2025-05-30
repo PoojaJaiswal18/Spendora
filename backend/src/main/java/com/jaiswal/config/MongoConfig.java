133f407712193e1e11ce3896836c609b063f98dc
Pooja Jaiswal <jaiswal.pooja1809@gmail.com>
2025-05-30 23:37:23 +0530

    add MongoConfig

--- backend/src/main/java/com/jaiswal/config/MongoConfig.java
+++ backend/src/main/java/com/jaiswal/config/MongoConfig.java
@@ -1,1 +1,62 @@
- 
+package com.jaiswal.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.database}")
+    private String databaseName;
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
+
+    @Override
+    @Bean
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(mongoUri);
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .applyToConnectionPoolSettings(builder ->
+                        builder.maxSize(20)
+                                .minSize(5)
+                                .maxWaitTime(2000, TimeUnit.MILLISECONDS)
+                                .maxConnectionLifeTime(0, TimeUnit.MILLISECONDS)
+                                .maxConnectionIdleTime(0, TimeUnit.MILLISECONDS))
+                .build();
+        return MongoClients.create(settings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        MongoTemplate template = new MongoTemplate(mongoClient(), getDatabaseName());
+        MappingMongoConverter converter = (MappingMongoConverter) template.getConverter();
+        converter.setTypeMapper(new DefaultMongoTypeMapper(null));
+        return template;
+    }
+
+    @Bean
+    public MongoCustomConversions customConversions() {
+        return new MongoCustomConversions(java.util.Collections.emptyList());
+    }
+}
+
