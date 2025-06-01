f07700d7458b4c9ce6c5237cfe977115be770014
Pooja Jaiswal <jaiswal.pooja1809@gmail.com>
2025-06-01 12:30:03 +0530

    add OCR utils

--- backend/src/main/java/com/jaiswal/util/OCRUtils.java
+++ backend/src/main/java/com/jaiswal/util/OCRUtils.java
@@ -1,1 +1,8 @@
- 
+package com.jaiswal.util;
+
+public class OCRUtils {
+    public static String cleanExtractedText(String text) {
+        return text.replaceAll("[^\\x00-\\x7F]", "").trim();
+    }
+}
+
