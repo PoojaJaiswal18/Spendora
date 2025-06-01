1c8593539c0c3be04c8342cd4641f9354af56cd1
Pooja Jaiswal <jaiswal.pooja1809@gmail.com>
2025-06-01 12:28:29 +0530

    add constant utils

--- backend/src/main/java/com/jaiswal/util/Constants.java
+++ backend/src/main/java/com/jaiswal/util/Constants.java
@@ -1,1 +1,11 @@
- 
+package com.jaiswal.util;
+
+public class Constants {
+    public static final String DEFAULT_CURRENCY = "USD";
+    public static final String DEFAULT_TIMEZONE = "UTC";
+    public static final int MAX_RECEIPT_UPLOAD_SIZE_MB = 10;
+    public static final String[] ALLOWED_RECEIPT_FILE_TYPES = {"jpg", "jpeg", "png", "pdf"};
+    public static final String RECEIPT_UPLOAD_DIR = "uploads/receipts";
+    private Constants() {}
+}
+
