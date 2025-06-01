d48f08c90046cd6893a4cb32ca99dd1e3915ef4d
Pooja Jaiswal <jaiswal.pooja1809@gmail.com>
2025-06-01 12:23:42 +0530

    add authException

--- backend/src/main/java/com/jaiswal/exception/AuthenticationException.java
+++ backend/src/main/java/com/jaiswal/exception/AuthenticationException.java
@@ -1,1 +1,11 @@
- 
+package com.jaiswal.exception;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
